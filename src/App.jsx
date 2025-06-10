d570fbe05c30b71d43c64015bb1c71ee7454c3e7
Abduhak1movAbdumaj1d <abduhakimovabdumajid632@gmail.com>
2025-06-10 09:54:16 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Abdumajid</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aut quos, voluptatibus nobis architecto nemo officiis labore ex repellendus impedit libero, id fuga hic esse, eligendi corporis a exercitationem nisi!</p>
+      <ul>
+        <li>nmadur</li>
+        <li>nmadur</li>
+        <li>nmadur</li>
+        <li>nmadur</li>
+        <li>nmadur</li>
+        <li>nmadur</li>
+        <li>nmadur</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
